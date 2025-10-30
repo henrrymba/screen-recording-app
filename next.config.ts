8f49cf970de60283423645ff555ae60f42e614f6
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-10-30 23:07:23 +0100

    feat(videos): fetch uploaded video data from Bunny.net

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "henrry-snapcast.b-cdn.net",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "50mb",
+        },
+    },
 };
 
 export default nextConfig;
